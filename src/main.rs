96b3f88048b3bd96cf70ac485b592276e87baf11
e5f6bd <41416412+e5f6bd@users.noreply.github.com>
2023-11-29 20:34:09 +0900

    LED blink

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+use rppal::gpio::Gpio;
+
+fn main() -> anyhow::Result<()> {
+    let gpio = Gpio::new()?;
+    let mut pin = gpio.get(26)?.into_output_low();
+    let secs = 60.0 / 300.0 / 2.;
+    loop {
+        pin.set_high();
+        std::thread::sleep(Duration::from_secs_f64(secs));
+        pin.set_low();
+        std::thread::sleep(Duration::from_secs_f64(secs));
+    }
+    // Ok(())
 }
